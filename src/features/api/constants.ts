698fb8467303e9c679962a26ac3a776375d71365
Yuriy <yura.posledov@yandex.ru>
2024-06-07 19:48:47 +0300

    feat: try to requests

--- src/features/api/constants.ts
+++ src/features/api/constants.ts
@@ -1,4 +1,4 @@
-export const WEBSERVICE_HOST = 'http://51.250.97.205:8080'
+export const WEBSERVICE_HOST = 'http://localhost:8080'
 export const WEBSERVICE_API_PREFIX = '/platform-api'
 export const MODULES = '/modules'
 export const LESSON = '/lessons'
@@ -6,7 +6,7 @@ export const STEPS = '/steps'
 export const CATEGORIES = '/categories'
 export const WORDS = '/words'
 
-export const STRAPI_HOST = 'http://51.250.97.205:1337'
+export const STRAPI_HOST = 'http://localhost:1337'
 export const STRAPI_API_PREFIX = '/api'
 export const NEIRO_URL = 'https://detect.roboflow.com/sign-language-recognition-mmbok/1?api_key=rbhBeux1LGVsZeGa8lpy'
 
