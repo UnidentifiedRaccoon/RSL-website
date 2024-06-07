bad2ab95bc2fa08b018156971543fe9dfd5aa7bd
Yuriy <yura.posledov@yandex.ru>
2024-06-07 21:12:04 +0300

    fix: project setup

--- src/features/api/constants.ts
+++ src/features/api/constants.ts
@@ -1,12 +1,11 @@
-export const WEBSERVICE_HOST = 'http://localhost:8080'
+export const WEBSERVICE_HOST = 'http://51.250.97.205:8080'
 export const WEBSERVICE_API_PREFIX = '/platform-api'
 export const MODULES = '/modules'
 export const LESSON = '/lessons'
 export const STEPS = '/steps'
 export const CATEGORIES = '/categories'
 export const WORDS = '/words'
-
-export const STRAPI_HOST = 'http://localhost:1337'
+export const STRAPI_HOST = 'http://51.250.97.205:1337'
 export const STRAPI_API_PREFIX = '/api'
 export const NEIRO_URL = 'https://detect.roboflow.com/sign-language-recognition-mmbok/1?api_key=rbhBeux1LGVsZeGa8lpy'
 
